94fdc15ba3b3cde05b5c03cf20308c811b970957
Lee Baesler <lee.baesler@gmail.com>
2022-09-16 11:54:43 -0500

    PUT request finished to update ready_to_transfer from yes to no

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 const pool = require('../modules/pool');
@@ -9,7 +10,17 @@ const pool = require('../modules/pool');
 
 
 // PUT
-
+koalaRouter.put('/:koalaid', function (req,res){
+    console.log('in PUT route', req.params, req.body);
+    const query = 'UPDATE "koalas" SET "ready_to_transfer"=$1 WHERE id=$2;';
+    pool.query(query, [req.body.ready_to_transfer, req.params.koalaid])
+    .then((results) => {
+        res.send(results.rows);
+    }).catch((error) => {
+        console.log('Error making PUT', error);
+        res.sendStatus(500);
+    });
+    });
 
 // DELETE
 
