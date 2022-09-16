513a3144a920c51ccda52409b355bbe0075e8a2d
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-16 11:06:23 -0500

    added pool boiler

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,1 +1,19 @@
 // DB CONNECTION
+const pg = require('pg');
+const pool = new pg.Pool({
+    database: 'koala_holla',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+    console.log('Connected to postgres');
+});
+
+pool.on('error', () => {
+    console.log('Error connecting to postgres');
+});
+
+module.exports = pool;
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 
-
 // GET
 
 
